4398993423f2898a795ff5071bffe7ee745bea19
Liam Falconer <liam@lfn3.net>
2019-08-07 07:36:54 +0100

    Revert "Start maintaining our own sequence."
    This reverts commit aba45d6b
    On second thought, this probably belongs in it's own, specialized
    type of DatabaseBackedLog

--- src/main/kotlin/net/lfn3/leth/jooq/DatabaseBackedLog.kt
+++ src/main/kotlin/net/lfn3/leth/jooq/DatabaseBackedLog.kt
@@ -6,7 +6,6 @@ import net.lfn3.leth.LogWriter
 import org.jooq.DSLContext
 import org.jooq.TableRecord
 import org.jooq.exception.DataAccessException
-import org.jooq.impl.DSL
 import java.sql.SQLException
 
 class DatabaseBackedLog<T, R : TableRecord<R>>(
@@ -14,16 +13,6 @@ class DatabaseBackedLog<T, R : TableRecord<R>>(
     private val dslProvider: () -> DSLContext,
     private val reader: DatabaseBackedLogReader<T, R> = DatabaseBackedLogReader(logWriterMappings.asReadonly(), dslProvider = dslProvider)
 ) : Log<T>, LogWriter<T>, LogReader<T> by reader {
-    // I know what you're thinking. Why is this here?
-    // We want to maintain the invariant that there aren't any gaps in the log ids
-    // Postgres makes this somewhat tricky, since it does not have transactional rollbacks around sequences, so you can pull
-    // a value from a sequence, conflict somewhere and then drop that value on the floor.
-    // This would make us sad.
-    // So, instead we maintain our own sequence in memory, and optimistically insert.
-    // This will, of course, create problems if there's multiple writers.
-    @Volatile
-    private var hwm : Long? = null
-
     override fun record(entry: T): Long {
         val dbr = logWriterMappings.toRecord.invoke(entry)
 
@@ -35,27 +24,19 @@ class DatabaseBackedLog<T, R : TableRecord<R>>(
     }
 
     private fun executeInsert(dbr: R): Long {
-        dslProvider().use { dsl ->
-            val nextHwm = getHwm(dsl) + 1
-
-            dbr[logWriterMappings.sequenceField] = nextHwm
-            //TODO: back off and retry when the seq conflicts (how do we tell it's that field that conflicted?)
-            dsl.insertInto(logWriterMappings.table)
+        return dslProvider().use { dsl ->
+            val query = dsl.insertInto(logWriterMappings.table)
                 .set(dbr)
-                .execute()
 
-            hwm = nextHwm
-            return nextHwm
-        }
-    }
+            if (logWriterMappings.supportsReturning) {
+                query.returning(logWriterMappings.sequenceField)
+                    .fetchOne()
+                    .get(logWriterMappings.sequenceField)
+            } else {
+                query.execute()
 
-    private fun getHwm(dsl: DSLContext) : Long {
-        return if (hwm == null) {
-            dsl.select(DSL.max(logWriterMappings.sequenceField))
-                .from(logWriterMappings.table)
-                .fetchOne()[DSL.max(logWriterMappings.sequenceField)] ?: 0
-        } else {
-            hwm!!
+                dsl.fetchValue("select scope_identity()") as Long
+            }
         }
     }
 
