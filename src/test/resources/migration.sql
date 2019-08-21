0da1ff0e3c53eceda572ddda28cbfe2bc606ebff
Liam Falconer <liam@lfn3.net>
2019-08-21 07:57:57 +0100

    Try out zero indexed table.
    This causes other problems. Might need to handle it the different index
    offsets in code. (It's fairly likely we'll have to deal with both cases
    in practice anyway?)

--- src/test/resources/migration.sql
+++ src/test/resources/migration.sql
@@ -1,5 +1,7 @@
+create sequence logged_seq start with 0;
+
 create table if not exists logged (
-  log_id bigint primary key auto_increment,
+  log_id bigint default logged_seq.nextval primary key,
   ancestor_id bigint unique references logged(log_id),
   value_one bigint,
   value_two bigint
